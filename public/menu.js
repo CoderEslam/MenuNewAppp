60d24897238c35f5a0c23a6b360e97f405855e49
eslam <eslamfgazy600@gmail.com>
2022-10-31 03:07:46 +0200

    Menu App

--- public/menu.js
+++ public/menu.js
@@ -1,6 +1,17 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
 import {
-    getDatabase, ref, set, onValue, get, child, query, orderByChild
+    getDatabase,
+    ref,
+    set,
+    onValue,
+    get,
+    child,
+    query,
+    orderByChild,
+    onChildAdded,
+    onChildChanged,
+    onChildRemoved,
+    limitToLast
 } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
 
 const firebaseConfig = {
@@ -19,12 +30,9 @@ var databaseref = getDatabase(app);
 var Menu = query(ref(databaseref, 'Menu'), orderByChild('index'));
 var Food = ref(databaseref, 'Food')
 
-var divTable = document.getElementById("table");
-var table = document.createElement("table");
 var menu;
 var food;
 var arrayMenu = [];
-var arrayFood = [];
 gerAllMenus();
 
 function gerAllMenus() {
@@ -32,10 +40,11 @@ function gerAllMenus() {
         menu = snapshot.val();
         var c = 0;
         for (let i in menu) {
-            arrayMenu[c] = {"id": menu[i].id, "name": menu[i].name}
+            arrayMenu[c] = {"id": menu[i].id, "name": menu[i].name, image: menu[i].image, index: menu[i].index}
             c++;
-            console.log(arrayMenu);
         }
+        // to sort array by index of menu
+        arrayMenu.sort((a, b) => a.index - b.index);
         getAllFood();
     });
 }
@@ -46,8 +55,14 @@ function getAllFood() {
         for (let i = 0; i < arrayMenu.length; i++) {
             var h1 = document.createElement("h1");
             h1.innerHTML = arrayMenu[i].name;
-            document.getElementById("table").appendChild(h1)
+            var table_div = document.getElementById("table_div");
+            table_div.appendChild(h1);
+            var bottom_r = document.getElementById("bottom-r");
+            // bottom_r.setAttribute('src', arrayMenu[i].image);
+            // var top_l = document.getElementById("top-l");
+            // top_l.setAttribute('src', arrayMenu[i].image);
             var table = document.createElement("table");
+            // table.setAttribute('background', 'image/bg2.jpg')
             var nameth = document.createElement("th");
             nameth.innerHTML = "Name"
             var decth = document.createElement("th");
@@ -68,40 +83,121 @@ function getAllFood() {
                 var cellText = document.createTextNode(childDataFood.name);
                 td.appendChild(cellText);
                 tr.appendChild(td);*/
-                if (arrayMenu[i].id === food[j].idMenu) {
-                    var tr = document.createElement("tr");
-                    var name = document.createElement("td");
-                    name.innerHTML = food[j].name;
-                    var details = document.createElement("td");
-                    // details.innerHTML = food[j].details;
-                    if (food[j].details.length > 20) {
-                        var SummaryDetails = document.createElement("details");
-                        var summary = document.createElement("summary");
-                        var p = document.createElement("p");
-                        SummaryDetails.appendChild(summary);
-                        SummaryDetails.appendChild(p);
-                        summary.innerHTML = food[j].details.substring(0, 15) + "...";
-                        p.innerHTML = food[j].details;
-                        details.appendChild(SummaryDetails);
-                    } else {
-                        details.innerHTML = food[j].details;
+                if (food[j].classification !== "VIP") {
+                    if (arrayMenu[i].id === food[j].idMenu) {
+                        var tr = document.createElement("tr");
+                        var name = document.createElement("td");
+                        name.innerHTML = food[j].name;
+                        var details = document.createElement("td");
+                        // details.innerHTML = food[j].details;
+                        if (food[j].details.length > 20) {
+                            var SummaryDetails = document.createElement("details");
+                            var summary = document.createElement("summary");
+                            var p = document.createElement("p");
+                            SummaryDetails.appendChild(summary);
+                            SummaryDetails.appendChild(p);
+                            summary.innerHTML = food[j].details.substring(0, 10) + "...";
+                            p.innerHTML = food[j].details;
+                            details.appendChild(SummaryDetails);
+                        } else {
+                            details.innerHTML = food[j].details;
+                        }
+                        var tdimg = document.createElement("td");
+                        var price = document.createElement("td");
+                        console.log(typeof food[j].priceSmall === 'undefined');
+                        var tablePrice = document.createElement("table");
+                        tablePrice.setAttribute('class', 'tablePrice');
+                        var small = document.createElement("th");
+                        small.setAttribute('class', 'thPrice');
+                        small.innerHTML = "Small"
+                        var Medium = document.createElement("th");
+                        Medium.setAttribute('class', 'thPrice');
+                        Medium.innerHTML = "Medium"
+                        var Large = document.createElement("th");
+                        Large.setAttribute('class', 'thPrice');
+                        Large.innerHTML = "Large"
+                        tablePrice.appendChild(small);
+                        tablePrice.appendChild(Medium);
+                        tablePrice.appendChild(Large);
+                        var trPrice = document.createElement("tr");
+                        var nameSmall = document.createElement("td");
+                        nameSmall.setAttribute('class', 'tdPrice');
+                        var nameMedium = document.createElement("td");
+                        nameMedium.setAttribute('class', 'tdPrice');
+                        var nameLarge = document.createElement("td");
+                        nameLarge.setAttribute('class', 'tdPrice');
+
+                        if ((food[j].priceSmall !== 0 && typeof food[j].priceSmall !== 'undefined') && (food[j].priceMedium !== 0 && typeof food[j].priceMedium !== 'undefined') && (food[j].priceLarge !== 0 && typeof food[j].priceLarge !== 'undefined')) {
+                            nameSmall.innerHTML = food[j].priceSmall;
+                            nameMedium.innerHTML = food[j].priceMedium;
+                            nameLarge.innerHTML = food[j].priceLarge;
+                            trPrice.appendChild(nameSmall);
+                            trPrice.appendChild(nameMedium);
+                            trPrice.appendChild(nameLarge);
+                            tablePrice.appendChild(trPrice);
+                            price.appendChild(tablePrice)
+                            // price.innerHTML = food[j].priceSmall + "" + "<sup>S</sup>" + "    " + food[j].priceMedium + "" + "<sup>M</sup>" + "    " + food[j].priceLarge + "" + "<sup>L</sup>";
+                        } else if (typeof food[j].priceSmall === 'undefined' && typeof food[j].priceMedium === 'undefined' && typeof food[j].priceLarge === 'undefined') {
+                            price.innerHTML = "there is no money" + " " + "<sup>Dh</sup>";
+                        } else if (food[j].priceSmall === 0 && (typeof food[j].priceMedium === 'undefined' || food[j].priceMedium === 0) && (typeof food[j].priceLarge === 'undefined' || food[j].priceLarge === 0)) {
+                            price.innerHTML = "there is no money" + " " + "<sup>Dh</sup>";
+                            // in small and Medium
+                        } else if ((food[j].priceSmall !== 0 && typeof food[j].priceSmall !== 'undefined') && (food[j].priceMedium !== 0 && typeof food[j].priceMedium !== 'undefined') && (typeof food[j].priceLarge === 'undefined' || food[j].priceLarge === 0)) {
+                            nameSmall.innerHTML = food[j].priceSmall;
+                            nameMedium.innerHTML = food[j].priceMedium;
+                            trPrice.appendChild(nameSmall);
+                            trPrice.appendChild(nameMedium);
+                            tablePrice.appendChild(trPrice);
+                            price.appendChild(tablePrice)
+                        } else if (food[j].priceSmall !== 0 && food[j].priceLarge !== 0 && (typeof food[j].priceMedium === 'undefined' || food[j].priceMedium === 0)) {
+                            nameSmall.innerHTML = food[j].priceSmall;
+                            nameLarge.innerHTML = food[j].priceLarge;
+                            trPrice.appendChild(nameSmall);
+                            trPrice.appendChild(nameLarge);
+                            tablePrice.appendChild(trPrice);
+                            price.appendChild(tablePrice)
+                        } else if ((food[j].priceSmall !== 0 && typeof food[j].priceSmall !== 'undefined') && (food[j].priceMedium === 0 || typeof food[j].priceMedium === 'undefined') && (food[j].priceLarge === 0 || typeof food[j].priceLarge === 'undefined')) {
+                            price.innerHTML = food[j].priceSmall + " " + "<sup>Dh</sup>";
+                        } else if ((food[j].priceSmall === 0 || typeof food[j].priceSmall === 'undefined') && (food[j].priceMedium === 0 || typeof food[j].priceMedium === 'undefined') && (food[j].priceLarge === 0 || typeof food[j].priceLarge === 'undefined')) {
+                            price.innerHTML = "there is no money" + " " + "<sup>Dh</sup>";
+                        }
+
+
+                        var img = document.createElement("img");
+                        img.setAttribute('src', food[j].image);
+                        tr.appendChild(name);
+                        tr.appendChild(details);
+                        tr.appendChild(price);
+                        tr.appendChild(tdimg);
+                        tdimg.appendChild(img)
+                        table.appendChild(tr);
+                        table_div.appendChild(table)
+                        // console.log(menu[i].name + " => " + food[j].name);
                     }
-                    var tdimg = document.createElement("td");
-                    var price = document.createElement("td");
-                    price.innerHTML = food[j].price + " " + "<sup>€</sup>";
-                    var img = document.createElement("img");
-                    img.setAttribute('src', food[j].image);
-                    tr.appendChild(name);
-                    tr.appendChild(details);
-                    tr.appendChild(price);
-                    tr.appendChild(tdimg);
-                    tdimg.appendChild(img)
-                    table.appendChild(tr);
-                    document.getElementById("table").appendChild(table)
-                    // console.log(menu[i].name + " => " + food[j].name);
                 }
+
             }
         }
 
     });
 }
+
+
+/*const commentsRef = query(ref(databaseref, 'Menu'), orderByChild('index'))
+onChildAdded(commentsRef, (data) => {
+    // var c = 0;
+    // for (let i in menu) {
+    // arrayMenu[data.val().id] = {"id": data.val().id, "name": data.val().name}
+    // c++;
+    // console.log(arrayMenu)
+    // }
+    // getAllFood();
+});
+
+onChildChanged(commentsRef, (data) => {
+    console.log(data.val());
+});
+
+onChildRemoved(commentsRef, (data) => {
+    console.log(data.val());
+});*/
